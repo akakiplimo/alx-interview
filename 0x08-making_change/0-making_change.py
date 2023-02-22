4d8335ea90e1ec524f063b24f4a378300f6d7ebf
abracodeabra <akakiplimo@gmail.com>
2023-02-22 08:41:31 +0000

    add statement for when total is 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,6 +9,13 @@ import sys
 
 
 def makeChange(coins, total):
+    """
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    """
+    if total <= 0:
+        return 0
 
     # table[i] will be storing the minimum
     # number of coins required for i value.
