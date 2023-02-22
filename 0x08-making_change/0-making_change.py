b1e32a2dc366670d9a1e30b6a045afc499078cfb
Adrian Kiplimo <adrian@novek.io>
2023-02-22 11:01:19 +0300

    improve algo performace

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,27 +3,39 @@
     determine the fewest number of coins needed to meet
     a given amount total.
 '''
+# A Dynamic Programming based Python3 program to
+# find minimum of coins to make a given change V
 import sys
-
+ 
 
 def makeChange(coins, total):
-    '''
-    Return: fewest number of coins needed to meet total
-    If total is 0 or less, return 0
-    If total cannot be met by any number of coins you have, return -1
-    '''
-    if total <= 0:
-        return 0
-    table = [sys.maxsize for i in range(total + 1)]
+     
+    # table[i] will be storing the minimum
+    # number of coins required for i value.
+    # So table[V] will have result
+    table = [0 for i in range(total + 1)]
+ 
+    # Base case (If given value V is 0)
     table[0] = 0
-    m = len(coins)
+ 
+    # Initialize all table values as Infinite
     for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
-
+        table[i] = sys.maxsize
+ 
+    # Compute minimum coins required
+    # for all values from 1 to V
+    for i in range(1, total + 1):
+         
+        # Go through all coins smaller than i
+        for j in range(len(coins)):
+            if (coins[j] <= i):
+                sub_res = table[i - coins[j]]
+                if (sub_res != sys.maxsize and
+                    sub_res + 1 < table[i]):
+                    table[i] = sub_res + 1
+     
     if table[total] == sys.maxsize:
         return -1
+       
     return table[total]
+
